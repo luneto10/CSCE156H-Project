f8db8e66fda91b6146d68bff537171df54f42b18
luneto10 <lguedesdecarvalhon2@huskers.unl.edu>
2024-01-25 13:07:12 -0600

    Testing bug in push

--- src/main/src/unl/soc/Main.java
+++ src/main/src/unl/soc/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Testing the github");
+        System.out.println("Testing the bug yhh");
     }
 }
