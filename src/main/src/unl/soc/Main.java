fc96235eda42d1143c3bbdf9b44eac6f8be4821b
lucascoelho_unl <lmoreiradesouzacoe2@huskers.unl.edu>
2024-02-12 13:36:17 -0600

    add: Lucas test git

--- src/main/src/unl/soc/Main.java
+++ src/main/src/unl/soc/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello World");
         System.out.println("Testing the github");
 
+        System.out.println("Testing github from Lucas");
     }
 }
